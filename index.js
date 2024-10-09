aa438db83b00fde44d78bc3d8e244070e962e005
shamanthgiddaluru <shamanthgiddaluru@gmail.com>
2024-10-09 15:20:21 +0530

    Added pattern printing function

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const findEvenNumbers = (arr) => arr.filter(num => num % 2 === 0);
 const findOddNumbers = (arr) => arr.filter(num => num % 2 !== 0);
 
+const printPattern = (rows) => {
+    for (let i = 1; i <= rows; i++) {
+        console.log('* '.repeat(i));
+    }
+};
+
 console.log("Even Numbers:", findEvenNumbers(numbers));
 console.log("Odd Numbers:", findOddNumbers(numbers));
+printPattern(5);
